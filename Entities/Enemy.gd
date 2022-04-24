ad3f83fcc9e2eaa09d081a21424d671e89c85777
alborrajo <alvarolopezborr@gmail.com>
2022-04-24 16:13:44 +0200

    Differently colored placemarks for HOBO enemies

--- Entities/Enemy.gd
+++ Entities/Enemy.gd
@@ -1,26 +1,103 @@
 extends Resource
 class_name Enemy
 
-var enemy_type: EnemyType
-var named_enemy_params_id: int = 0x8FA
-var raid_boss_id: int = 0
-var scale: int = 100
-var lv: int = 10
-var hm_preset_no: int = 0
-var start_think_tbl_no: int = 0
-var repop_num: int = 0
-var repop_count: int = 0
-var enemy_target_types_id: int = 1
-var montage_fix_no: int = 0
-var set_type: int = 0
-var infection_type: int = 0
-var is_boss_gauge: bool = false
-var is_boss_bgm: bool = false
-var is_manual_set: bool = false
-var is_area_boss: bool = false
-var is_blood_enemy: bool = false
-var is_highorb_enemy: bool = false
+var enemy_type: EnemyType setget _set_enemy_type
+var named_enemy_params_id: int = 0x8FA setget _set_named_enemy_params_id
+var raid_boss_id: int = 0 setget _set_raid_boss_id
+var scale: int = 100 setget _set_scale
+var lv: int = 10 setget _set_lv
+var hm_preset_no: int = 0 setget _set_hm_preset_no
+var start_think_tbl_no: int = 0 setget _set_start_think_tbl_no
+var repop_num: int = 0 setget _set_repop_num
+var repop_count: int = 0 setget _set_repop_count
+var enemy_target_types_id: int = 1 setget _set_enemy_target_types_id
+var montage_fix_no: int = 0 setget _set_montage_fix_no
+var set_type: int = 0 setget _set_set_type
+var infection_type: int = 0 setget _set_infection_type
+var is_boss_gauge: bool = false setget _set_is_boss_gauge
+var is_boss_bgm: bool = false setget _set_is_boss_bgm
+var is_manual_set: bool = false setget _set_is_manual_set
+var is_area_boss: bool = false setget _set_is_area_boss
+var is_blood_enemy: bool = false setget _set_is_blood_enemy
+var is_highorb_enemy: bool = false setget _set_is_highorb_enemy
 
 func _init(type: EnemyType):
 	self.enemy_type = type
 	self.hm_preset_no = type.default_hm_preset_no
+
+func _set_enemy_type(value):
+	enemy_type = value
+	emit_changed()
+	
+func _set_named_enemy_params_id(value):
+	named_enemy_params_id = value
+	emit_changed()
+	
+func _set_raid_boss_id(value):
+	raid_boss_id = value
+	emit_changed()
+	
+func _set_scale(value):
+	scale = value
+	emit_changed()
+	
+func _set_lv(value):
+	lv = value
+	emit_changed()
+	
+func _set_hm_preset_no(value):
+	hm_preset_no = value
+	emit_changed()
+	
+func _set_start_think_tbl_no(value):
+	start_think_tbl_no = value
+	emit_changed()
+	
+func _set_repop_num(value):
+	repop_num = value
+	emit_changed()
+	
+func _set_repop_count(value):
+	repop_count = value
+	emit_changed()
+	
+func _set_enemy_target_types_id(value):
+	enemy_target_types_id = value
+	emit_changed()
+	
+func _set_montage_fix_no(value):
+	montage_fix_no = value
+	emit_changed()
+	
+func _set_set_type(value):
+	set_type = value
+	emit_changed()
+	
+func _set_infection_type(value):
+	infection_type = value
+	emit_changed()
+	
+func _set_is_boss_gauge(value):
+	is_boss_gauge = value
+	emit_changed()
+	
+func _set_is_boss_bgm(value):
+	is_boss_bgm = value
+	emit_changed()
+	
+func _set_is_manual_set(value):
+	is_manual_set = value
+	emit_changed()
+	
+func _set_is_area_boss(value):
+	is_area_boss = value
+	emit_changed()
+	
+func _set_is_blood_enemy(value):
+	is_blood_enemy = value
+	emit_changed()
+	
+func _set_is_highorb_enemy(value):
+	is_highorb_enemy = value
+	emit_changed()
+	
--- UI/Marker/EnemyPlacemark.gd
+++ UI/Marker/EnemyPlacemark.gd
@@ -1,6 +1,10 @@
 extends Button
 class_name EnemyPlacemark
 
+const COLOR_BLOOD_ORB = Color.rebeccapurple
+const COLOR_HIGH_ORB = Color.darkred
+const COLOR_DEFAULT = Color.white
+
 export (Resource) var enemy: Resource setget set_enemy
 
 onready var _enemyDetails: EnemyDetails = EnemyDetails.get_instance(get_tree()) 
@@ -19,5 +23,20 @@ func _on_EnemyPlacemark_pressed():
 	_enemyDetails.enemy = enemy
 	
 func set_enemy(em: Enemy) -> void:
-	text = "%s (Lv. %d)" % [em.enemy_type.name, em.lv]
+	if enemy != null and enemy.is_connected("changed", self, "_on_enemy_changed"):
+		enemy.disconnect("changed", self, "_on_enemy_changed")
+		
 	enemy = em
+		
+	em.connect("changed", self, "_on_enemy_changed")
+	_on_enemy_changed()
+	
+func _on_enemy_changed():
+	text = "%s (Lv. %d)" % [enemy.enemy_type.name, enemy.lv]
+	
+	if enemy.is_blood_enemy:
+		modulate = COLOR_BLOOD_ORB
+	elif enemy.is_highorb_enemy:
+		modulate = COLOR_HIGH_ORB
+	else: 
+		modulate = COLOR_DEFAULT
