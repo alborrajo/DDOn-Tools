3f6c4980a9d2d869e9eda8fc5404ed0e9a26ac62
alborrajo <alvarolopezborr@gmail.com>
2022-05-09 20:36:46 +0200

    Store enemy sets from unknown stage layouts
    If data from a set not known by the program is found when loading a file
    it'll be stored in a variable so it can be added back to the file when
    saving in order to prevent losing data

--- UI/FileMenu.gd
+++ UI/FileMenu.gd
@@ -36,6 +36,8 @@ export (NodePath) var notification_popup: NodePath
 
 var _file_path: String setget _set_file_path
 
+var _unknown_stage_layout_sets := []
+
 onready var file_dialog_node: FileDialog = get_node(file_dialog)
 onready var enemy_tree_node: EnemyTree = get_node(enemy_tree)
 onready var notification_popup_node: NotificationPopup = get_node(notification_popup)
@@ -45,13 +47,18 @@ func _ready():
 	
 func _on_markers_loaded():
 	var file_path = StorageProvider.get(STORAGE_KEY_FILE_PATH)
-	if file_path != null:
+	if file_path != null and file_path != "":
 		_do_load(file_path)
 
 func _unhandled_input(event: InputEvent):
 	if Input.is_key_pressed(KEY_CONTROL) and event.is_pressed() and event is InputEventKey:
 		var inputEventKey := event as InputEventKey
-		if inputEventKey.scancode == KEY_S:
+		if inputEventKey.scancode == KEY_N:
+			# New
+			_do_new()
+			get_tree().set_input_as_handled()
+		elif inputEventKey.scancode == KEY_S:
+			# Save
 			if _file_path != "":
 				_do_save(_file_path)
 				get_tree().set_input_as_handled()
@@ -60,6 +67,7 @@ func _unhandled_input(event: InputEvent):
 				print(err_message)
 				notification_popup_node.notify(err_message)
 		elif inputEventKey.scancode == KEY_L:
+			# Load
 			if _file_path != "":
 				_do_load(_file_path)
 				get_tree().set_input_as_handled()
@@ -71,10 +79,20 @@ func _unhandled_input(event: InputEvent):
 func _on_menu_id_pressed(id: int) -> void:
 	match id:
 		0:
-			_on_load()
+			_on_new()
 		1:
+			_on_load()
+		2:
 			_on_save()
 			
+func _on_new():
+	_do_new()
+	
+func _do_new():
+	print_debug("New file. Clearing workspace")
+	_clean()
+	self._file_path = ""
+
 func _on_load():
 	file_dialog_node.mode = FileDialog.MODE_OPEN_FILE
 	file_dialog_node.popup()
@@ -104,10 +122,7 @@ func _do_load(file_path: String):
 			return
 	
 	# First clean current data
-	for node in get_tree().get_nodes_in_group("EnemyPlacemark"):
-		if node is EnemySetPlacemark:
-			var placemark := node as EnemySetPlacemark
-			placemark.clear_enemies()
+	_clean()
 	
 	# Then load it from the file
 	while !file.eof_reached():
@@ -124,6 +139,7 @@ func _do_load(file_path: String):
 			
 		# Inefficient af, but i can't be assed
 		# Storing the CSV data in a hashmap first or similar would be better
+		var is_unknown_stage_layout := true
 		for node in get_tree().get_nodes_in_group("EnemyPlacemark"):
 			if node is EnemySetPlacemark:
 				var placemark := node as EnemySetPlacemark
@@ -149,7 +165,16 @@ func _do_load(file_path: String):
 					enemy.is_blood_enemy = parse_bool(csv_line[21])
 					enemy.is_highorb_enemy = parse_bool(csv_line[22])
 					placemark.add_enemy(enemy)
+					is_unknown_stage_layout = false
 					break
+					
+		# If a EnemySetPlacemark couldn't be found for this row
+		# add to the unknown stage layout data list
+		# This data will later be saved back to the file to prevent losing
+		# data not managed by this program
+		if is_unknown_stage_layout:
+			print_debug("Enemy %s found in unknown set: %s,%s,%s,%s\n\tIt won't be shown on the map" % [csv_line[4], csv_line[0], csv_line[1], csv_line[2], csv_line[3]])
+			_unknown_stage_layout_sets.append(csv_line)
 	
 	file.close()
 	
@@ -198,16 +223,37 @@ func _do_save(file_path: String):
 				csv_data.append(enemy.is_blood_enemy)
 				csv_data.append(enemy.is_highorb_enemy)
 				file.store_csv_line(csv_data)
+				
+	# Add back data for unknown stage layouts to prevent losing data
+	# not managed by this program
+	for csv_data in _unknown_stage_layout_sets:
+		print_debug("Saving %s in unknown set: %s,%s,%s,%s" % [csv_data[4], csv_data[0], csv_data[1], csv_data[2], csv_data[3]])
+		file.store_csv_line(csv_data)
+	
 	file.close()
 
 	self._file_path = file_path
 	
 	notification_popup_node.notify("Saved file "+file_path)
+	
+
+func _clean() -> void:
+	_unknown_stage_layout_sets = []
+	for node in get_tree().get_nodes_in_group("EnemyPlacemark"):
+		if node is EnemySetPlacemark:
+			var placemark := node as EnemySetPlacemark
+			placemark.clear_enemies()
+
 
 func _set_file_path(file_path: String) -> void:
 	_file_path = file_path
-	OS.set_window_title("DDOn Tools - "+file_path)
 	StorageProvider.set(STORAGE_KEY_FILE_PATH, _file_path)
+	
+	# TODO: Move this somewhere else to handle the title via signals
+	if file_path == "":
+		OS.set_window_title("DDOn Tools")
+	else:
+		OS.set_window_title("DDOn Tools - "+file_path)
 	
 static func parse_bool(string: String) -> bool:
 	return string.strip_edges().to_lower() == "true"
