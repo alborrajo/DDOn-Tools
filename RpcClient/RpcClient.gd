d19390891f111d6c8d1611269439947179499512
alborrajo <alvarolopezborr@gmail.com>
2022-09-19 22:40:00 +0200

    RpcClient query params support

--- RpcClient/RpcClient.gd
+++ RpcClient/RpcClient.gd
@@ -10,7 +10,9 @@ const STORAGE_KEY_RPC_HOST_DEFAULT := "localhost"
 const STORAGE_KEY_RPC_PORT := "port"
 const STORAGE_KEY_RPC_PORT_DEFAULT := 52099
 const STORAGE_KEY_RPC_PATH := "path"
-const STORAGE_KEY_RPC_PATH_DEFAULT := "/rpc/info"
+const STORAGE_KEY_RPC_PATH_DEFAULT := "/rpc/"
+
+const RPC_PATH_INFO = "info"
 
 func _init():
 	pass
@@ -20,11 +22,13 @@ func _ready():
 	pass
 
 func get_info() -> Array:
+	return _get_array(RPC_PATH_INFO)
+func _get_array(relative_path: String, query_params: Dictionary = {}) -> Array:
 	var host = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_HOST, STORAGE_KEY_RPC_HOST_DEFAULT)
 	var port = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_PORT, STORAGE_KEY_RPC_PORT_DEFAULT)
-	var path = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_PATH, STORAGE_KEY_RPC_PATH_DEFAULT)
-	var res = make_request(host, port, path)
-	print("RpcClient: get_info ", host,":",port, path) 
+	var base_path = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_PATH, STORAGE_KEY_RPC_PATH_DEFAULT)
+	var path = str(base_path, relative_path)
+	var res = make_request(host, port, path, query_params)
 	if typeof(res) != TYPE_ARRAY:
 		print("RpcClient: expected Json Array") 
 		# {} = Dictionary 
@@ -32,11 +36,14 @@ func get_info() -> Array:
 		return Array()
 	return res
 
-func make_request(var host : String, var port: int = 80, var path: String = '/'):
+func make_request(host: String, port: int = 80, path: String = '/', query_params: Dictionary = {}):
 	var err = 0
 	var http = HTTPClient.new()
 	err = http.connect_to_host(host, port)
 	
+	var query_string = http.query_string_from_dict(query_params)
+	print("RpcClient: ", host,":",port, path, "?", query_string) 
+		
 	if err != OK:
 		printerr("RpcClient: failed to establish connection ", host,":",port) 
 		return
@@ -51,20 +58,21 @@ func make_request(var host : String, var port: int = 80, var path: String = '/')
 			OS.delay_msec(HTTP_DELAY_MS)
 		try_count = try_count + 1
 		if try_count >= 10:
-			printerr("RpcClient: STATUS_CONNECTING try_count >= 10 ", host,":",port, path) 
+			printerr("RpcClient: STATUS_CONNECTING try_count >= 10 ", host,":",port,path,"?",query_string) 
 			return
 			
 	if http.get_status() != HTTPClient.STATUS_CONNECTED:
-		printerr("RpcClient: failed to connect ", host,":",port, path) 
+		printerr("RpcClient: failed to connect ", host,":",port, path,"?",query_string) 
 		return
 		
 	var headers = [
 		"User-Agent: Pirulo/1.0 (Godot)",
 		"Accept: */*"
 	]
-	err = http.request(HTTPClient.METHOD_GET, path, headers)
+	
+	err = http.request(HTTPClient.METHOD_GET, str(path,"?",query_string), headers)
 	if err != OK:
-		printerr("RpcClient: request failed ", host,":",port, path) 
+		printerr("RpcClient: request failed ", host,":",port, path,"?",query_string) 
 		return
 		
 	try_count = 0
@@ -76,20 +84,20 @@ func make_request(var host : String, var port: int = 80, var path: String = '/')
 			OS.delay_msec(HTTP_DELAY_MS)
 		try_count = try_count + 1
 		if try_count >= 10:
-			printerr("RpcClient: STATUS_REQUESTING try_count >= 10 ", host,":",port, path) 
+			printerr("RpcClient: STATUS_REQUESTING try_count >= 10 ", host,":",port, path,"?",query_string) 
 			return
 			
 	if http.get_status() != HTTPClient.STATUS_BODY and http.get_status() != HTTPClient.STATUS_CONNECTED:
-		printerr("RpcClient: failed ", host,":",port, path) 
+		printerr("RpcClient: failed ", host,":",port, path,"?",query_string) 
 		return
 		
 	if !http.has_response():
-		printerr("RpcClient: no response ", host,":",port, path) 
+		printerr("RpcClient: no response ", host,":",port, path,"?",query_string) 
 		return
 
 	headers = http.get_response_headers_as_dictionary()
 	if http.get_response_code() != 200:
-		printerr("RpcClient: bad response code ", host,":",port, path) 
+		printerr("RpcClient: bad response code ", host,":",port, path,"?",query_string) 
 		return
 		
 	try_count = 0
@@ -104,7 +112,7 @@ func make_request(var host : String, var port: int = 80, var path: String = '/')
 				OS.delay_usec(HTTP_DELAY_MS)
 			try_count = try_count + 1
 			if try_count >= 10:
-				printerr("RpcClient: STATUS_BODY try_count >= 10 ", host,":",port, path) 
+				printerr("RpcClient: STATUS_BODY try_count >= 10 ", host,":",port, path,"?",query_string) 
 				return
 		else:
 			rb = rb + chunk
@@ -112,6 +120,6 @@ func make_request(var host : String, var port: int = 80, var path: String = '/')
 	var body : String = rb.get_string_from_utf8()
 	var json = JSON.parse(body)
 	if json.error != OK:
-		printerr("RpcClient: failed to parse json ", host,":",port, path) 
+		printerr("RpcClient: failed to parse json ", host,":",port, path,"?",query_string) 
 		return
 	return json.result
