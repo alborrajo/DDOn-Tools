b3cfb3956c134dc2552c21e102514834b969c61b
alborrajo <alvarolopezborr@gmail.com>
2022-09-21 00:05:36 +0200

    Chat post message support

--- RpcClient/RpcClient.gd
+++ RpcClient/RpcClient.gd
@@ -30,21 +30,31 @@ func get_chat(since_iso_date: String = "") -> Array:
 		return _get_array(RPC_PATH_CHAT)
 	else:
 		return _get_array(RPC_PATH_CHAT, {"since": since_iso_date})
+		
+func post_chat(chat_message_log_entry: Dictionary) -> void:
+	_post_dictionary(RPC_PATH_CHAT, chat_message_log_entry)
 	
 func _get_array(relative_path: String, query_params: Dictionary = {}) -> Array:
 	var host = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_HOST, STORAGE_KEY_RPC_HOST_DEFAULT)
 	var port = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_PORT, STORAGE_KEY_RPC_PORT_DEFAULT)
 	var base_path = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_PATH, STORAGE_KEY_RPC_PATH_DEFAULT)
 	var path = str(base_path, relative_path)
-	var res = make_request(host, port, path, query_params)
+	var res = make_request(HTTPClient.METHOD_GET, host, port, path, query_params)
 	if typeof(res) != TYPE_ARRAY:
 		print("RpcClient: expected Json Array") 
 		# {} = Dictionary 
 		# [] = Array
 		return Array()
 	return res
+	
+func _post_dictionary(relative_path: String, body: Dictionary) -> void:
+	var host = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_HOST, STORAGE_KEY_RPC_HOST_DEFAULT)
+	var port = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_PORT, STORAGE_KEY_RPC_PORT_DEFAULT)
+	var base_path = StorageProvider.get_value(STORAGE_SECTION_RPC, STORAGE_KEY_RPC_PATH, STORAGE_KEY_RPC_PATH_DEFAULT)
+	var path = str(base_path, relative_path)
+	var res = make_request(HTTPClient.METHOD_POST, host, port, path, {}, JSON.print(body))
 
-func make_request(host: String, port: int = 80, path: String = '/', query_params: Dictionary = {}):
+func make_request(method: int, host: String, port: int = 80, path: String = '/', query_params: Dictionary = {}, body: String = ""):
 	var err = 0
 	var http = HTTPClient.new()
 	err = http.connect_to_host(host, port)
@@ -78,7 +88,7 @@ func make_request(host: String, port: int = 80, path: String = '/', query_params
 		"Accept: */*"
 	]
 	
-	err = http.request(HTTPClient.METHOD_GET, str(path,"?",query_string), headers)
+	err = http.request(method, str(path,"?",query_string), headers, body)
 	if err != OK:
 		printerr("RpcClient: request failed ", host,":",port, path,"?",query_string) 
 		return
@@ -104,7 +114,7 @@ func make_request(host: String, port: int = 80, path: String = '/', query_params
 		return
 
 	headers = http.get_response_headers_as_dictionary()
-	if http.get_response_code() != 200:
+	if http.get_response_code() < 200 or http.get_response_code() >= 300:
 		printerr("RpcClient: bad response code ", host,":",port, path,"?",query_string) 
 		return
 		
@@ -125,8 +135,8 @@ func make_request(host: String, port: int = 80, path: String = '/', query_params
 		else:
 			rb = rb + chunk
 		
-	var body : String = rb.get_string_from_utf8()
-	var json = JSON.parse(body)
+	var response_body : String = rb.get_string_from_utf8()
+	var json = JSON.parse(response_body)
 	if json.error != OK:
 		printerr("RpcClient: failed to parse json ", host,":",port, path,"?",query_string) 
 		return
--- UI/Chat.gd
+++ UI/Chat.gd
@@ -1,18 +1,72 @@
 extends VBoxContainer
 class_name Chat
 
+const MAX_ENTRIES := 100 # TODO: Configurable
+
 var _rpc_client := RpcClient.new()
 
+var _is_at_bottom := false
 var _last_request_iso_date: String = ""
 
 func _ready():
-	_on_RPCTimer_timeout()
+	_on_Chat_visibility_changed()
+	$ChatLog.get_v_scroll().value = $ChatLog.get_v_scroll().max_value
+	$ChatLog.get_v_scroll().connect("changed", self, "_on_chatlog_vscroll_changed")
 
 func _on_RPCTimer_timeout():
-	var since_iso_date := _last_request_iso_date
+	$RPCTimer.paused = true
+	_is_at_bottom = $ChatLog.get_v_scroll().value+$ChatLog.get_v_scroll().page == $ChatLog.get_v_scroll().max_value
+	var iso_date := _last_request_iso_date
 	_last_request_iso_date = str(Time.get_datetime_string_from_system(true),".",OS.get_system_time_msecs(),"Z") # UTC date
-	var chatlog := _rpc_client.get_chat(since_iso_date)
+	var chatlog := _rpc_client.get_chat(iso_date)
 	if chatlog.size() > 0:
 		print(chatlog.size(), " new chat messages")
 		for logentry in chatlog:
-			$ChatLog.add_item("%s %s: %s" % [logentry["FirstName"], logentry["LastName"], logentry["ChatMessage"]["Message"]])
+			if _is_duplicate_msg(logentry):
+				printerr("Duplicate message: ",logentry)
+			else:
+				var datetime_dict := Time.get_datetime_dict_from_datetime_string(logentry["DateTime"], false)
+				$ChatLog.add_item("[%02d:%02d] %s %s: %s" % [datetime_dict["hour"], datetime_dict["minute"], logentry["FirstName"], logentry["LastName"], logentry["ChatMessage"]["Message"]])
+				$ChatLog.set_item_metadata($ChatLog.get_item_count()-1, logentry)
+	while $ChatLog.get_item_count() > MAX_ENTRIES:
+		$ChatLog.remove_item(0)
+	$RPCTimer.paused = false
+
+func _on_MessageLineEdit_text_entered(message: String):
+	$ChatBox/MessageLineEdit.editable = false
+	var chat_message_log_entry := {
+		# DateTime not used
+		# TODO: FirstName
+		# TODO: LastName
+		# TODO: CharacterId
+		"ChatMessage": {
+			"Type": $ChatBox/MsgTypeOptionButton.get_item_text($ChatBox/MsgTypeOptionButton.selected),
+			"Message": message,
+			"Deliver": true
+		}
+	}
+	_rpc_client.post_chat(chat_message_log_entry)
+	$ChatBox/MessageLineEdit.clear()
+	$ChatBox/MessageLineEdit.editable = true
+	_on_RPCTimer_timeout()
+
+func _on_Chat_visibility_changed():
+	if visible:
+		_on_RPCTimer_timeout()
+		$RPCTimer.start()
+	else:
+		$RPCTimer.stop()
+
+func _on_chatlog_vscroll_changed(value = null):
+	# Scroll back to bottom if it already was before updating
+	if _is_at_bottom:
+		$ChatLog.get_v_scroll().value = $ChatLog.get_v_scroll().max_value
+
+# Inefficient af but i didnt find a better way
+func _is_duplicate_msg(new_msg: Dictionary) -> bool:
+	if $ChatLog.get_item_count() > 0:
+		for msg_idx in range($ChatLog.get_item_count()-1, -1, -1): # Check in reverse
+			var msg: Dictionary = $ChatLog.get_item_metadata(msg_idx)
+			if new_msg["CharacterId"] == msg["CharacterId"] and new_msg["FirstName"] == msg["FirstName"] and new_msg["LastName"] == msg["LastName"] and new_msg["DateTime"] == msg["DateTime"] and new_msg["ChatMessage"]["Type"] == msg["ChatMessage"]["Type"] and new_msg["ChatMessage"]["Message"] == msg["ChatMessage"]["Message"] and new_msg["ChatMessage"]["Deliver"] == msg["ChatMessage"]["Deliver"]:
+				return true
+	return false
