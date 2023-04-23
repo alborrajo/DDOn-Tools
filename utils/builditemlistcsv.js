9bd7957a86fa60f0e5b4b58f819e1ce77ebdffb8
alborrajo <alvarolopezborr@gmail.com>
2023-04-23 16:53:46 +0200

    updated itemlist.ipa stuff

--- utils/builditemlistcsv.js
+++ utils/builditemlistcsv.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const itemlist = require('../resources/itemlist.json');
 
-const csvHeader = ['#ItemId','Category','Price'];
+const csvHeader = ['#ItemId','Category','Price','StackLimit'];
 
 const csvData = [
-    ...itemlist.consumables.map(c => [c.info.itemId, c.info.category, c.price ?? 0]),
-    ...itemlist.materials.map(c => [c.info.itemId, c.info.category, c.price ?? 0]),
-    ...itemlist.jobItems.map(c => [c.info.itemId, c.info.category, c.price ?? 0]),
-    ...itemlist.jobItems.map(c => [c.info.itemId, c.info.category, c.price ?? 0]),
-    ...itemlist.specialItems.map(c => [c.info.itemId, c.info.category, c.price ?? 0]),
-    ...itemlist.weapons.map(c => [c.itemId, c.category, c.price ?? 0]),
-    ...itemlist.armors.map(c => [c.itemId, c.category, c.price ?? 0]),
-    ...itemlist.jewelries.map(c => [c.itemId, c.category, c.price ?? 0]),
-    ...itemlist.npcEquipments.map(c => [c.itemId, c.category, c.price ?? 0])];
+    ...itemlist.consumables.map(c => [c.info.itemId, c.info.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.materials.map(c => [c.info.itemId, c.info.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.jobItems.map(c => [c.info.itemId, c.info.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.jobItems.map(c => [c.info.itemId, c.info.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.specialItems.map(c => [c.info.itemId, c.info.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.weapons.map(c => [c.itemId, c.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.armors.map(c => [c.itemId, c.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.jewelries.map(c => [c.itemId, c.category, c.price ?? 0, c.stackLimit ?? 1]),
+    ...itemlist.npcEquipments.map(c => [c.itemId, c.category, c.price ?? 0, c.stackLimit ?? 1])];
 
 const csvLines = [csvHeader.join(','), ...csvData.map(line => line.join(','))];
 const serialized = csvLines.join('\n');
