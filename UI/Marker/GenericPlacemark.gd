9071486c2799502fc8cade6e4f935c940d4e3989
meecube <42190924+meecube@users.noreply.github.com>
2024-05-04 17:44:05 +0200

    cleaned up selectionvisible toggling

--- UI/Marker/GenericPlacemark.gd
+++ UI/Marker/GenericPlacemark.gd
@@ -9,7 +9,7 @@ func ready():
 	selectionpanel = get_node("SelectionPanel")
 
 func _clear_all():
-	remove_highlight(selectionpanel)
+	selectionpanel.visible = false
 
 func _gui_input(event):
 	if event is InputEventMouseButton and (event as InputEventMouseButton).button_mask == BUTTON_RIGHT:
@@ -24,19 +24,9 @@ func receive_selection_panel(selectionpanel):
 func selection_function(type):
 	if Input.is_key_pressed(KEY_SHIFT):
 		SelectedListManager._toggle_selection(self, type)  # Pass EnemyPlacemark instance and enemy data
-		if selectionpanel.visible:
-			remove_highlight(selectionpanel)
-		else:
-			add_highlight(selectionpanel)
+		if selectionpanel:
+			selectionpanel.visible = !selectionpanel.visible
 	else:
 		SelectedListManager._clear_list()
 		SelectedListManager._toggle_selection(self, type)
-		add_highlight(selectionpanel)
-
-func remove_highlight(selectionpanel):
-	if selectionpanel:
-		selectionpanel.visible = false
-
-func add_highlight(selectionpanel):
-	if selectionpanel:
 		selectionpanel.visible = true
