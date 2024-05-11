1ad93c23b80f5db02d9b228c19e3eaffb54c6ea9
meecube <42190924+meecube@users.noreply.github.com>
2024-05-11 15:18:11 +0200

    adjusted the hiding behaviour to be transparent instead

--- StageItemList.gd
+++ StageItemList.gd
@@ -2,20 +2,13 @@ extends ItemList
 class_name StagesItemList
 
 func _ready():
-	self.clear()
-	var stage_no_list = DataProvider.repo["StageEctMarkers"].keys()
-	for stage_no in stage_no_list:
-		var stage_id = DataProvider.stage_no_to_stage_id(int(stage_no))
-		if stage_id == -1:
-			add_item("Stage No. "+stage_no) # TODO: i18n
-		else:
-			add_item("%s [%s]" % [tr(str("STAGE_NAME_",stage_id)), stage_id])
-		set_item_metadata(get_item_count()-1, stage_no)
-
+	update_stage_list()
 
 func _on_StagesLineEdit_text_changed(new_text: String):
-	new_text = new_text.to_lower() # Normalize to lowercase for case insensitivity
-	clear()
+	update_stage_list(new_text)
+
+func update_stage_list(filter_text: String = ""):
+	self.clear()
 	var stage_no_list = DataProvider.repo["StageEctMarkers"].keys()
 	for stage_no in stage_no_list:
 		var stage_id = DataProvider.stage_no_to_stage_id(int(stage_no))
@@ -25,7 +18,7 @@ func _on_StagesLineEdit_text_changed(new_text: String):
 		else:
 			item_text = "%s [%s]" % [tr(str("STAGE_NAME_", stage_id)), stage_id]
 
-		# Check if the item text matches the input text
-		if new_text == "" or item_text.to_lower().find(new_text) != -1:
-			add_item(item_text) # Add item if it matches
-			set_item_metadata(get_item_count()-1, stage_no)
+		# Check if the item text matches the filter text
+		if filter_text == "" or item_text.to_lower().find(filter_text.to_lower()) != -1:
+			add_item(item_text)  # Add item if it matches
+			set_item_metadata(get_item_count() - 1, stage_no)
