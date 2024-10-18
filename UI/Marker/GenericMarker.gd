dcf29a3ff11052c60479747f25f25e45776d95a1
alborrajo <alvarolopezborr@gmail.com>
2024-10-18 16:49:27 +0200

    Scale UI elements x1000 to allow more precision
    Instead of trying to always use map coordinates

--- UI/Marker/GenericMarker.gd
+++ UI/Marker/GenericMarker.gd
@@ -5,12 +5,8 @@ onready var _original_zoom : float = get_tree().get_nodes_in_group("camera")[0].
 onready var _original_scale := scale/_original_zoom
 
 func set_pos_vec(var vec: Vector2):
-	set_pos(vec.x, vec.y)
-
-func set_pos(var x, var z):
-	position.x = x
-	position.y = z
+	position = vec * Main.MAP_SCALE
 
 func _process(_delta):
 	var camera_zoom: float = get_tree().get_nodes_in_group("camera")[0].zoom.x
-	scale = _original_scale * clamp(camera_zoom, 0, _original_zoom)
+	scale = _original_scale * clamp(camera_zoom, 0, _original_zoom) * Main.MAP_SCALE
