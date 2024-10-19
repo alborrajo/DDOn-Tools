1227e40487d713a66dc7d6e7132f127ca9f6c2e2
alborrajo <alvarolopezborr@gmail.com>
2024-10-19 14:35:12 +0200

    Enemy group placemarks that unroll into position placemarks
    Middle click to collapse back into the group placemark

---
+++ UI/Marker/EnemyGroupPlacemark.gd
@@ -0,0 +1,68 @@
+extends Node
+class_name EnemyGroupPlacemark
+
+signal group_mouse_entered(position_index, enemy_position_placemark)
+signal group_mouse_exited(position_index, enemy_position_placemark)
+
+const EnemyPositionPlacemarkScene = preload("res://UI/Marker/EnemyPositionPlacemark.tscn")
+
+var enemy_set: EnemySet
+
+onready var _original_zoom : float = get_tree().get_nodes_in_group("camera")[0].original_zoom
+onready var _button_original_scale: Vector2 = $GroupButton.rect_scale
+
+func _ready():
+	enemy_set.connect("changed", self, "_on_enemy_set_changed")
+	_on_enemy_set_changed()
+	
+	for position_index in enemy_set.positions.size():
+		var enemy_position: EnemyPosition = enemy_set.positions[position_index]
+		var map_entity = MapEntity.new(enemy_position.coordinates, DataProvider.stage_id_to_stage_no(enemy_set.stage_id))
+		var enemy_position_placemark: EnemyPositionPlacemark = EnemyPositionPlacemarkScene.instance()
+		enemy_position_placemark.rect_position = map_entity.get_map_position()*1000
+		enemy_position_placemark.connect("mouse_entered", self, "_on_enemy_position_placemark_mouse_entered", [position_index, enemy_position_placemark])
+		enemy_position_placemark.connect("mouse_exited", self, "_on_enemy_position_placemark_mouse_exited", [position_index, enemy_position_placemark])
+		enemy_position_placemark.enemy_position = enemy_position
+		enemy_position_placemark.connect("gui_input", self, "_on_EnemyPositionPlacemark_gui_input")
+		$EnemyPositionPlacemarksControl.add_child(enemy_position_placemark)
+	
+	# TODO: Calcualte group center instead of using the first position
+	$GroupButton.rect_position = $EnemyPositionPlacemarksControl.get_child(0).rect_position
+
+func _process(_delta):
+	var camera_zoom: float = get_tree().get_nodes_in_group("camera")[0].zoom.x
+	$GroupButton.rect_scale = _button_original_scale * clamp(camera_zoom, 0, _original_zoom)
+	
+func _on_enemy_set_changed():
+	$GroupButton.text = "%d - %d/%d" % [enemy_set.group_id, enemy_set.effective_enemy_count(), enemy_set.positions.size()]
+
+func _on_GroupButton_mouse_entered():
+	emit_signal("group_mouse_entered", -1, null)
+
+func _on_GroupButton_mouse_exited():
+	emit_signal("group_mouse_exited", -1, null)
+	
+func _on_enemy_position_placemark_mouse_entered(position_index: int, enemy_position_placemark: EnemyPositionPlacemark):
+	emit_signal("group_mouse_entered", position_index, enemy_position_placemark)
+	
+func _on_enemy_position_placemark_mouse_exited(position_index: int, enemy_position_placemark: EnemyPositionPlacemark):
+	emit_signal("group_mouse_exited", position_index, enemy_position_placemark)
+
+func _on_GroupButton_pressed():
+	show_positions()
+	
+func _on_EnemyPositionPlacemark_gui_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_MIDDLE and event.pressed:
+			hide_positions()
+
+func get_position_placemarks() -> Array:
+	return $EnemyPositionPlacemarksControl.get_children()
+	
+func show_positions() -> void:
+	$GroupButton.visible = false
+	$EnemyPositionPlacemarksControl.visible = true
+	
+func hide_positions() -> void:
+	$GroupButton.visible = true
+	$EnemyPositionPlacemarksControl.visible = false
