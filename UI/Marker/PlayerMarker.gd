14a8bc84c150fce7a14f00d22418b3eb4f2b7363
alborrajo <alvarolopezborr@gmail.com>
2025-02-28 18:38:40 +0100

    Show player names on the map

--- UI/Marker/PlayerMarker.gd
+++ UI/Marker/PlayerMarker.gd
@@ -6,6 +6,7 @@ var player : PlayerMapEntity
 func set_player(var p_player : PlayerMapEntity):
 	player = p_player
 	set_pos_vec(player.get_map_position())
+	$PlayerNameLabel.text = "%s %s" % [player.FirstName, player.LastName]
 	
 func _on_ui_stage_selected(new_map_stage_no):
 	visible = int(new_map_stage_no) == player.StageNo
