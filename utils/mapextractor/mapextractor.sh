cb19aa93f39bb95ed6d862813192fd4b5052ef51
alborrajo <alvarolopezborr@gmail.com>
2025-03-29 16:47:43 +0100

    Util to extract icons

--- utils/mapextractor/mapextractor.sh
+++ utils/mapextractor/mapextractor.sh
@@ -10,9 +10,30 @@ fi
 
 cd "$(dirname "$0")"
 
+mkdir -p icons
 mkdir -p results
 mkdir -p tmp
 cd tmp
+
+# Copy item icon files and unpack them
+for arc in "$2"/nativePC/rom/ui/00_common/Item/*.arc
+do
+    arccopy=$(basename "$arc")
+    
+    echo "Copying $arccopy"
+    cp "$arc" .
+    
+    echo "Unarchiving $arccopy"
+    "$1" -ddo -pc -tex -v 7 "$arccopy"
+
+    icon=`find "${arccopy%.*}" -type f -name "*.dds"`
+    iconbasename=`basename "$icon"`
+    echo "Converting item icon $iconbasename to png"
+    magick convert "$icon" "../icons/${iconbasename%.*}.png"
+
+    # Clean up arc file
+    rm "$arccopy"
+done
 
 # Copy map files and unpack them
 for arc in "$2"/nativePC/rom/ui/02_map/map/**/*.arc
