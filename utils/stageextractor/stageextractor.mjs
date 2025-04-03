cd2b3a95a328d5c5489e865b0a30f6528276f060
alborrajo <alvarolopezborr@gmail.com>
2025-04-03 18:51:15 +0200

    Changed stageextractor to use ddon-data repo

--- utils/stageextractor/stageextractor.mjs
+++ utils/stageextractor/stageextractor.mjs
@@ -5,6 +5,12 @@ import JSON5 from 'json5';
 import { $, cd, chalk, echo } from "zx";
 import { exit } from "process";
 
+const gatheringSpotOmTypes = [
+    4, // cSetInfoOmGather
+    7, // cSetInfoOmTreasureBox
+    42 // SetInfoOmTreasureBoxG
+];
+
 /**
  * Target rLayout file name format:
  * stXXXX_00m00n_pYY
@@ -13,115 +19,72 @@ import { exit } from "process";
  */
 const regex = /[a-z]+([0-9]{4})_[0-9]{2}m[0-9]{2}n_.([0-9]+)/;
 
-if(argv._.length < 2) {
-    echo("Usage: ./stageextractor.mjs \"/path/to/ARCtool\" \"/path/to/Dragon's Dogma Online\"");
+if(argv._.length < 1) {
+    echo("Usage: ./stageextractor.mjs \"/path/to/ddon-data\"");
+    echo("Obtain ddon-data from here: https://github.com/ddon-research/ddon-data/");
     echo("To use this you must have ImHex installed in your PATH");
     exit(1);
 }
 
-const arcToolPath = argv._[0];
-const ddonPath = argv._[1];
+const ddonDataPath = argv._[0];
 
-mkdir('tmp', {recursive: true});
-cd('tmp');
+mkdir('out', {recursive: true});
+cd('out');
 
 // Find gathering spots
+echo("Looking for stage layout files...");
 const stageNoAndGatheringSpots = new Map();
-const stLotArcPaths = (await $`find ${ddonPath} -path "*/lot/*" -type f -name "*.arc"`.quiet()).stdout.split('\n').filter(filename => filename.trim().length > 0);
-for (const arcPath of stLotArcPaths) {
-    const parsedArcPath = path.parse(arcPath);
-    
-    echo(`Copying ${parsedArcPath.base}`);
-    await cp(arcPath, parsedArcPath.base);
-    
-    echo(`Unarchiving ${parsedArcPath.base}`);
-    await $`${arcToolPath} -ddo -pc -tex -v 7 ${parsedArcPath.base}`.quiet();
-
-    const pLayoutPaths = (await $`find ${parsedArcPath.name} -type f -name "${parsedArcPath.name}_p*.15302EF4"`.quiet()).stdout.split('\n').filter(filename => filename.trim().length > 0);
-    if (pLayoutPaths.length == 0) {
-        echo(`No _p rLayout files in ${parsedArcPath.name}`)
-    } else {
-        for(const pLayoutPath of pLayoutPaths) {
-            const [stageNo, groupNo, lot] = await readLayout(pLayoutPath);
-            for (let index = 0; index < lot.setInfo.length; index++) {
-                const setInfo = lot.setInfo[index];
-                if(!stageNoAndGatheringSpots.has(stageNo)) {
-                    stageNoAndGatheringSpots.set(stageNo, new Array());
+const stLotPaths = (await $`find ${ddonDataPath}/client/03040008 -type f -name "st*_p*.lot.json"`.quiet()).stdout.split('\n').filter(filename => filename.trim().length > 0);
+for (const stLotPath of stLotPaths) {
+    echo(`Reading layout ${stLotPath}`);
+    const [stageNo, groupNo, lot] = await readLayout(stLotPath);
+    echo(`\t(StageNo ${stageNo}, GroupNo ${groupNo})`);
+    const gatheringSpots = [];
+    for (let index = 0; index < lot.Array.length; index++) {
+        const setInfo = lot.Array[index];
+        if(gatheringSpotOmTypes.includes(setInfo.Type)) {
+            gatheringSpots.push({
+                GroupNo: groupNo,
+                PosId: Number(setInfo.Id),
+                GatheringType: setInfo.Info.GatheringType,
+                UnitId: setInfo.Info.InfoOm.InfoCoord.UnitID,
+                Position: {
+                    x: Number(setInfo.Info.InfoOm.InfoCoord.Position.X),
+                    y: Number(setInfo.Info.InfoOm.InfoCoord.Position.Y),
+                    z: Number(setInfo.Info.InfoOm.InfoCoord.Position.Z)
                 }
-                if(setInfo.type == 4) {
-                    stageNoAndGatheringSpots.get(stageNo).push({
-                        GroupNo: groupNo,
-                        PosId: Number(setInfo.id),
-                        Position: {
-                            x: Number(setInfo.info.SetInfoOm.SetInfoCoord.Position.x),
-                            y: Number(setInfo.info.SetInfoOm.SetInfoCoord.Position.y),
-                            z: Number(setInfo.info.SetInfoOm.SetInfoCoord.Position.z)
-                        },
-                        GatheringType: setInfo.info.GatheringType
-                    });
-                } else if(setInfo.type == 7) {
-                    stageNoAndGatheringSpots.get(stageNo).push({
-                        GroupNo: groupNo,
-                        PosId: Number(setInfo.id),
-                        Position: {
-                            x: Number(setInfo.info.super_cSetInfoOmGather.SetInfoOm.SetInfoCoord.Position.x),
-                            y: Number(setInfo.info.super_cSetInfoOmGather.SetInfoOm.SetInfoCoord.Position.y),
-                            z: Number(setInfo.info.super_cSetInfoOmGather.SetInfoOm.SetInfoCoord.Position.z)
-                        },
-                        GatheringType: setInfo.info.GatheringType
-                    });
-                } else if (setInfo.type == 42) {
-                    stageNoAndGatheringSpots.get(stageNo).push({
-                        GroupNo: groupNo,
-                        PosId: Number(setInfo.id),
-                        Position: {
-                            x: Number(setInfo.info.SetInfoOm.SetInfoCoord.Position.x),
-                            y: Number(setInfo.info.SetInfoOm.SetInfoCoord.Position.y),
-                            z: Number(setInfo.info.SetInfoOm.SetInfoCoord.Position.z)
-                        },
-                        GatheringType: setInfo.info.GatheringType
-                    });
-                }
-            }
+            });
         }
     }
-
-    echo(`Cleaning up ${parsedArcPath.base}`);
-    await rm(parsedArcPath.base);
-    await rm(parsedArcPath.name, { recursive: true });
+    echo(`\tFound ${gatheringSpots.length} gathering spots`);
+    if(gatheringSpots.length > 0) {
+        if(!stageNoAndGatheringSpots.has(stageNo)) {
+            stageNoAndGatheringSpots.set(stageNo, new Array());
+        }
+        stageNoAndGatheringSpots.get(stageNo).push(...gatheringSpots);
+    }
 }
-
 await writeFile('gatheringSpots.json', JSON.stringify(Object.fromEntries(stageNoAndGatheringSpots)));
 echo("All extracted p rLayout data written to gatheringSpots.json");
 
 // Find stage parts
+echo("Looking for stage parts files...");
 const stageNoAndParts = new Map();
-const stArcPaths = (await $`find ${ddonPath} -path "*/nativePC/rom/stage/st*" -type f -name "st????.arc"`.quiet()).stdout.split('\n').filter(filename => filename.trim().length > 0);
-for (const arcPath of stArcPaths) {
-    const parsedArcPath = path.parse(arcPath);
-    echo(`Copying ${parsedArcPath.base}`);
-    await cp(arcPath, parsedArcPath.base);
-    echo(`Unarchiving ${parsedArcPath.base} ${arcToolPath} -ddo -tex -alwayscomp -pc -v 7 ${parsedArcPath.base}`);
-    await $`${arcToolPath} -ddo -tex -alwayscomp -pc -v 7 ${parsedArcPath.base}`.quiet();
-
-    const stageCustomPaths = (await $`find ${parsedArcPath.name} -type f -name "st????.59F75535"`.quiet()).stdout.split('\n').filter(filename => filename.trim().length > 0);
-    if (stageCustomPaths.length == 0) {
-        echo(`No rStageCustom files in ${parsedArcPath.name}`)
+const stageCustomPaths = (await $`find ${ddonDataPath}/client/03040008 -type f -name "st????.sca.json"`.quiet()).stdout.split('\n').filter(filename => filename.trim().length > 0);
+for (const stageCustomPath of stageCustomPaths) {
+    echo(`Reading ${stageCustomPath}`);
+    const parsedStageCustomPath = path.parse(stageCustomPath);
+    const stageNo = Number(basename(parsedStageCustomPath.base).match(/st([0-9]{4})/)[1]);
+    const parsedStageCustom = await readStageCustom(stageCustomPath);
+    if(stageNoAndParts.has(stageNo)) {
+        echo(chalk.yellow(`Warning: Found more than one rStageCustom file for stage ${stageNo}. Ignoring`));
     } else {
-        const stageNo = Number(basename(parsedArcPath.base).match(/st([0-9]{4})/)[1]);
-        if (stageCustomPaths.length > 1) {
-            echo(chalk.yellow(`More than one rStageCustom file in ${parsedArcPath.name}. Reading only the first one`))
-        }
-        const stageCustomPath = stageCustomPaths[0];
-        const parsedStageCustom = await readStageCustom(stageCustomPath);
-        stageNoAndParts.set(stageNo, parsedStageCustom);
+        stageNoAndParts.set(stageNo, {
+            PartsPath: parsedStageCustom.PartsPath,
+            ArrayArea: parsedStageCustom.ArrayArea.map(area => area.AreaNo)
+        });
     }
-
-    echo(`Cleaning up ${parsedArcPath.base}`);
-    await rm(parsedArcPath.base);
-    await rm(parsedArcPath.name, { recursive: true });
 }
-
 await writeFile('stageCustom.json', JSON.stringify(Object.fromEntries(stageNoAndParts)));
 echo("All extracted rStageCustom data written to stageCustom.json");
 
@@ -130,17 +93,10 @@ echo("Done");
 
 async function readLayout(layoutPath) {
     const [stageNo, groupNo] = basename(layoutPath).match(regex).slice(1).map(Number);
-    echo(`Reading ${layoutPath} (StageNo ${stageNo}, GroupNo ${groupNo})`);
-    await $`imhex --pl format --pattern ../lot.hexpat --input ${layoutPath} --output tmp.json`.quiet();
-    const lot = JSON5.parse(String(await readFile("tmp.json", {encoding: "utf-8"})))
-    await rm("tmp.json");
+    const lot = JSON5.parse(String(await readFile(layoutPath, {encoding: "utf-8"})));
     return [stageNo, groupNo, lot];
 }
 
 async function readStageCustom(stageCustomPath) {
-    echo(`Reading ${stageCustomPath}`);
-    await $`imhex --pl format --pattern ../rstagecustom-season2-pattern.hexpat --input ${stageCustomPath} --output tmp.json`.quiet();
-    const stageCustomEx = JSON5.parse(String(await readFile("tmp.json", {encoding: "utf-8"})))
-    await rm("tmp.json");
-    return stageCustomEx;
+    return JSON5.parse(String(await readFile(stageCustomPath, {encoding: "utf-8"})))
 }
