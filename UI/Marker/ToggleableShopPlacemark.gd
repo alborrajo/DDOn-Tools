7d33474a3512a22b9d917c739a2b625ef505e9d6
alborrajo <alvarolopezborr@gmail.com>
2025-04-25 18:36:23 +0200

    Have the shop placemark use the SelectedListManager

--- UI/Marker/ToggleableShopPlacemark.gd
+++ UI/Marker/ToggleableShopPlacemark.gd
@@ -29,6 +29,8 @@ var coordinates: Vector3 setget _set_coordinates
 var shop: Shop setget _set_shop
 
 func _ready():
+	assert(SelectedListManager.connect("selection_changed", self, "_on_selection_changed") == OK)
+	assert(SelectedListManager.connect("selection_deleted", self, "_on_selection_deleted") == OK)
 	assert(SelectedListManager.connect("item_filter_changed", self, "_on_item_filter_changed") == OK)
 
 func get_display_name() -> String:
@@ -91,11 +93,36 @@ func _draw_tree_item(tree_item: TreeItem, rect: Rect2):
 	$MapControl/Control/Panel/ShopPlacemark/ScrollContainer/Tree.draw_string(get_font("font"), rect.position + Vector2(48, rect.size.y/2 + 16), "%s[%d]" % ["★".repeat(good.item.quality_stars), good.item.id], color)
 	$MapControl/Control/Panel/ShopPlacemark/ScrollContainer/Tree.draw_string(get_font("font"), rect.position + Vector2(148, rect.size.y/2 + 16), "%d %s" % [good.price, wallet_type], color)
 
-func _on_ItemList_item_rmb_selected(position: Vector2):
-	var shop_item: ShopItem = $MapControl/Control/Panel/ShopPlacemark/ScrollContainer/Tree.get_item_at_position(position).get_metadata(0)
-	var index := shop.get_goods().find(shop_item)
-	assert(index != -1)
-	shop.call_deferred("remove_goods", index)
+func _on_selection_changed(added: Array, removed: Array) -> void:
+	var root: TreeItem =$MapControl/Control/Panel/ShopPlacemark/ScrollContainer/Tree.get_root()
+	var shop_good_tree_item := root.get_children()
+	while shop_good_tree_item != null:
+		var shop_good: ShopItem = shop_good_tree_item.get_metadata(0)
+		if added.has(shop_good):
+			shop_good_tree_item.select(0)
+		elif removed.has(shop_good):
+			shop_good_tree_item.deselect(0)
+		shop_good_tree_item = shop_good_tree_item.get_next()
+	$MapControl/Control/Panel/ShopPlacemark/ScrollContainer/Tree.update() #rerender
+	
+func _on_selection_deleted(deleted: Array) -> void:
+	var goods := shop.get_goods()
+	# Go in reverse order because indexes
+	for index in range(goods.size() - 1, -1, -1):
+		var good: ShopItem = goods[index]
+		if deleted.has(good):
+			shop.remove_goods(index)
+
+func _on_Tree_multi_selected(item: TreeItem, column: int, selected: bool):
+	assert(column == 0)
+	var shop_item: ShopItem = item.get_metadata(column)
+	if selected:
+		SelectedListManager.add_to_selection(shop_item)
+	else:
+		SelectedListManager.remove_from_selection(shop_item)
+	
+func _on_Tree_item_rmb_selected(_position: Vector2):
+	SelectedListManager.call_deferred("delete_selected")
 
 func _on_ItemList_dragged_shop_item(shop_item):
 	var index := shop.get_goods().find(shop_item)
