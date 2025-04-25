6a728afe8c06cb925c7ddc707906ac5bb7c9d7ab
alborrajo <alvarolopezborr@gmail.com>
2025-04-26 00:10:16 +0200

    Fix ctrl + click not selecting multiple shop items

--- MapCoordinateSpace.gd
+++ MapCoordinateSpace.gd
@@ -6,32 +6,29 @@ var drag_start_global_position: Vector2
 
 func _input(event):
 	if visible:
-		if event is InputEventMouseButton:
-			var iemb := event as InputEventMouseButton
-			if iemb.button_index == BUTTON_LEFT:
-				if iemb.control and iemb.pressed:
+		if event is InputEventMouseMotion:
+			var iemm := event as InputEventMouseMotion
+			if iemm.control and iemm.button_mask == BUTTON_LEFT:
+				if not dragging:
 					dragging = true
 					drag_start_global_position = get_global_mouse_position()
-					get_tree().set_input_as_handled()
-				elif dragging:
-					dragging = false
-					if not iemb.shift:
-						SelectedListManager.clear_list()
-					_select_recursively(_get_selection_rect().abs(), self)
-					update()
-					get_tree().set_input_as_handled()
-		elif dragging and event is InputEventMouseMotion:
-			update()
-			get_tree().set_input_as_handled()
+				update()
+				get_tree().set_input_as_handled()
+			elif dragging:
+				dragging = false
+				if not iemm.shift:
+					SelectedListManager.clear_list()
+				_select_recursively(_get_selection_rect().abs(), self)
+				update()
+				get_tree().set_input_as_handled()
 
 func _draw():
 	if dragging:
 		var rect := _get_selection_rect()
 		draw_rect(rect, Color(0.27451, 0.509804,0.705882, 0.25), true)
-		#VisualServer.canvas_item_set_custom_rect(get_canvas_item(), true, rect)
+		VisualServer.canvas_item_set_custom_rect(get_canvas_item(), true, rect)
 	else:
-		pass
-		#VisualServer.canvas_item_set_custom_rect(get_canvas_item(), false)
+		VisualServer.canvas_item_set_custom_rect(get_canvas_item(), false)
 		
 func _select_recursively(selection_rect: Rect2, node: Node) -> void:
 	for child in node.get_children():
