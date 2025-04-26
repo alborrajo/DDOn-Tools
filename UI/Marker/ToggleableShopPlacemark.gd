2407afb54b4fec20235ecfd30c4d7bd93b45c227
alborrajo <alvarolopezborr@gmail.com>
2025-04-26 18:03:00 +0200

    Rerender shop items tree when its details change

--- UI/Marker/ToggleableShopPlacemark.gd
+++ UI/Marker/ToggleableShopPlacemark.gd
@@ -75,6 +75,12 @@ func _on_shop_changed() -> void:
 		tree_item.set_metadata(0, good)
 		tree_item.set_cell_mode(0, TreeItem.CELL_MODE_CUSTOM)
 		tree_item.set_custom_draw(0, self, "_draw_tree_item")
+		
+		if not good.is_connected("changed", self, "_on_shop_item_changed"):
+			good.connect("changed", self, "_on_shop_item_changed", [good])
+		
+func _on_shop_item_changed(shop_item: ShopItem):
+	$MapControl/Control/Panel/ShopPlacemark/ScrollContainer/Tree.update() #rerender
 
 func _draw_tree_item(tree_item: TreeItem, rect: Rect2):
 	var good: ShopItem = tree_item.get_metadata(0)
