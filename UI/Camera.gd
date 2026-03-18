46ec1bea7b7600a32a4e6c8878cae6b18c4f24d7
alborrajo <alvarolopezborr@gmail.com>
2026-03-18 23:56:55 +0100

    Fix panning by dragging mouse

--- UI/Camera.gd
+++ UI/Camera.gd
@@ -9,33 +9,26 @@ extends Camera2D
 
 func _unhandled_input(event):
 	# Otherwise, unhandled input doesn't take zoom into account when panning
-	if _handle_mouse(event, zoom.x):
+	if _handle_mouse(event):
 		get_viewport().set_input_as_handled()
 
 func _on_root_gui_input(event):
 	# warning-ignore:return_value_discarded
 	_handle_mouse(event)
 	
-func _handle_mouse(event, multiplier: float = 1) -> bool:
+func _handle_mouse(event) -> bool:
 	# Mouse pan
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not get_tree().root.gui_is_dragging():
-		# global_position -= event.relative * multiplier
-		global_position -= event.screen_relative * multiplier / zoom / DisplayServer.screen_get_scale()
+		global_position -= event.relative/zoom
 		return true
 	
 	# Mouse zoom
 	if event is InputEventMouseButton and event.is_pressed():
 		match event.button_index:
 			MOUSE_BUTTON_WHEEL_UP:
-				# Godot 4 migration
-				# mouse wheel events were changed to match the physical wheel movement instead of the logical scroll direction
-				# zoom *= 1-scroll_speed
 				zoom *= 1+scroll_speed
 				return true
 			MOUSE_BUTTON_WHEEL_DOWN:
-				# Godot 4 migration
-				# mouse wheel events were changed to match the physical wheel movement instead of the logical scroll direction
-				# zoom *= 1+scroll_speed
 				zoom *= 1-scroll_speed
 				return true
 	
