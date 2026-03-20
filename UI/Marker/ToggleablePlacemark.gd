084b81733252566b9fdf789e281ed07e953d277b
alborrajo <alvarolopezborr@gmail.com>
2026-03-20 01:41:22 +0100

    Fix middle mouse button click

--- UI/Marker/ToggleablePlacemark.gd
+++ UI/Marker/ToggleablePlacemark.gd
@@ -5,9 +5,6 @@ signal subgroup_mouse_entered()
 signal subgroup_mouse_exited()
 
 func _on_Control_mouse_entered():
-	# Godot 4 migration
-	# Node.raise() -> CanvasItem.move_to_front()
-	# raise()
 	move_to_front()
 	emit_signal("subgroup_mouse_entered")
 
@@ -15,9 +12,6 @@ func _on_Control_mouse_exited():
 	emit_signal("subgroup_mouse_exited")
 
 func _on_ToggleButton_mouse_entered():
-	# Godot 4 migration
-	# Node.raise() -> CanvasItem.move_to_front()
-	# raise()
 	move_to_front()
 	emit_signal("subgroup_mouse_entered")
 
@@ -32,19 +26,13 @@ func _on_ToggleButton_pressed():
 
 func _on_Control_gui_input(event: InputEvent):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_MIDDLE and event.button_pressed:
+		if event.button_index == MOUSE_BUTTON_MIDDLE and event.is_pressed():
 			_hide()
 
-# Godot 4 migration
-# The method "show()" overrides a method from native class "CanvasItem". This won't be called by the engine and may not work as expected.
-# func show() -> void:
 func _show() -> void:
 	$MapControl/ToggleButton.visible = false
 	$MapControl/Control.visible = true
 
-# Godot 4 migration
-# The method "show()" overrides a method from native class "CanvasItem". This won't be called by the engine and may not work as expected.
-# func hide() -> void:
 func _hide() -> void:
 	$MapControl/ToggleButton.visible = true
 	$MapControl/Control.visible = false
